f5ba22634b90bae7df993a22c135b4dcb461f716
Sam Hartman <hartmans@debian.org>
2015-03-12 12:33:50 -0400

    Schema updates; exclude expired keys from view

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
-create table if not exists psk_keys (keyid text primary key, key blob, client_dh_pub raw(20), key expiration timestamp);
+create table if not exists psk_keys (keyid text primary key, key blob, client_dh_pub raw(20), key_expiration timestamp);
 create table if not exists authorizations( client_dh_pub raw(20), coi string, acceptor_realm string, hostname string, apc string);
 create index if not exists authorizations_dhpub on authorizations( client_dh_pub);
-CREATE VIEW if not exists authorizations_keys as select keyid, authorizations.* from psk_keys join authorizations on psk_keys.client_dh_pub = authorizations.client_dh_pub;
+CREATE VIEW if not exists authorizations_keys as select keyid, authorizations.* from psk_keys join authorizations on psk_keys.client_dh_pub = authorizations.client_dh_pub and datetime(key_expiration) < datetime('now');
  
 
 .quit
