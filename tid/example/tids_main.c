f2d437db8b7cc910ec507f5266a58756826a508b
Alejandro Perez <alex.perez-mendez@jisc.ac.uk>
2019-03-04 15:14:43 +0100

    Add the "auto" value for the TIDS options

--- tid/example/tids_main.c
+++ tid/example/tids_main.c
@@ -379,20 +379,20 @@ int main (int argc,
   argp_parse(&argp, argc, argv, 0, 0, &opts);
 
   /* set default hostname if not passed */
-  if (strcmp(opts.hostname, "") == 0) {
+  if (strcmp(opts.hostname, "") == 0 || strcmp(opts.hostname, "auto") == 0) {
     opts.hostname = malloc(1024);
     gethostname(opts.hostname, 1024);
   }
 
   /* set ip address if not passed */
-  if (strcmp(opts.ip_address, "") == 0) {
+  if (strcmp(opts.ip_address, "") == 0 strcmp(opts.ip_address, "auto") == 0) {
     struct hostent *he = gethostbyname(opts.hostname);
     if (he != NULL) {
       opts.ip_address = inet_ntoa(*((struct in_addr*) he->h_addr_list[0]));
     }
   }
 
-  if (strcmp(opts.database_name, "") == 0)
+  if (strcmp(opts.database_name, "") == 0 || strcmp(opts.database_name, "auto") == 0)
     opts.database_name = "/var/lib/trust_router/keys";
 
   if (opts.port == 0)
